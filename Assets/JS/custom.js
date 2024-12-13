5f98efac541734e1d89dd0dd523ba3f3bbb7a313
TahmidAhmedKhan <tahmid4k@gmail.com>
2024-12-14 02:16:44 +0600

    finished  adding js for area calculation

--- Assets/JS/custom.js
+++ Assets/JS/custom.js
@@ -1,92 +1,67 @@
+/* getInputValue function */
+function inputValueOfId(inputId) {
+  const imputValue = parseFloat(document.getElementById(inputId).value);
+  return imputValue;
+}
+
+/* setInnerText function */
+function setInnerTextById(elementId, areaValue) {
+  const areaDisplay = document.getElementById(elementId);
+  areaDisplay.innerHTML = "Area (A) = " + areaValue + "cm<sup>2</sup>";
+}
+
 /* Triangle function */
 function calculateAreaTriangle() {
+  const triangleBase = inputValueOfId("triangle_base");
+  const triangleHeight = inputValueOfId("triangle_height");
+  const triangleArea = 0.5 * triangleBase * triangleHeight;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("triangle_area", triangleArea);
 }
 
 /* Rectangle function */
 function calculateAreaRectangle() {
+  const rectangleWidth = inputValueOfId("rectangle_width");
+  const rectangleLength = inputValueOfId("rectangle_length");
+  const rectangleArea = rectangleWidth * rectangleLength;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("rectangle_area", rectangleArea);
 }
 
 /* Parallelogram function */
 function calculateAreaParallelogram() {
+  const parallelogramBase = inputValueOfId("parallelogram_base");
+  const parallelogramHeight = inputValueOfId("parallelogram_height");
+  const parallelogramArea = parallelogramBase * parallelogramHeight;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("parallelogram_area", parallelogramArea);
 }
 
 /* Rhombus function */
 function calculateAreaRhombus() {
+  const rhombusD1 = inputValueOfId("rhombus_d1");
+  const rhombusD2 = inputValueOfId("rhombus_d2");
+  const rhombusArea = 0.5 * rhombusD1 * rhombusD2;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("rhombus_area", rhombusArea);
 }
 
 /* Pentagon function */
 function calculateAreaPentagon() {
+  const pentagonPerimeter = inputValueOfId("pentagon_perimeter");
+  const pentagonApothem = inputValueOfId("pentagon_apothem");
+  const pentagonArea = 0.5 * pentagonPerimeter * pentagonApothem;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("pentagon_area", pentagonArea);
 }
 
 /* Ellipse function */
 function calculateAreaEllipse() {
+  const ellipseSr = inputValueOfId("ellipse_sr");
+  const ellipseLr = inputValueOfId("ellipse_lr");
+  const ellipseArea = 3.14159 * ellipseSr * ellipseLr;
 
-    console.log("Button clicked!");
-
-    const triangleBase = parseFloat(document.getElementById("triangle_base").value);
-    const triangleHeight = parseFloat(document.getElementById("triangle_height").value);
-    const triangleArea = 0.5 * triangleBase * triangleHeight;
-
-    console.log("Triangle area:", triangleArea);
-    const triangleAreaDisplay = document.getElementById("triangle_area");
-    triangleAreaDisplay.innerHTML = "Area (A) = <span>" + triangleArea + "</span>cm<sup>2</sup>";
-
+  setInnerTextById("ellipse_area", ellipseArea);
 }
-
 
 console.log("connected");
